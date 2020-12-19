5ebeddd911520ca48f8b5ec12a3ad73c2671490d
minjulee97 <aamj0226@gmail.com>
2020-12-19 18:31:23 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/MyQnaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQnaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQnaWebsiteApplication.class, args);
+	}
+
+}
