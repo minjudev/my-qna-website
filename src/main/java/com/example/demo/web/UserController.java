c310a95916f4241c42fd4d7ab887faa2ef800780
minjulee97 <aamj0226@gmail.com>
2021-01-06 11:48:38 +0900

    add: 회원가입, 사용자 목록 확인 기능 추가

---
+++ src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController { 
+	private List<User> users = new ArrayList<User>();
+	
+	@PostMapping("/create")
+	public String create(User user) {
+		System.out.println("User: " + user);
+		users.add(user);
+		
+		return "redirect:/list";
+	}
+
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		System.out.println("users:" + users);
+		
+		return "list";
+	}
+}
